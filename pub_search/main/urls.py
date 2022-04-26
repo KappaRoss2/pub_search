31a027c8f8e050be5585fb2237af6e81640dd694
KappaRoss <vam.vam.10@mail.ru>
2022-04-26 19:29:59 +0500

    add register form

--- pub_search/main/urls.py
+++ pub_search/main/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name = "home"),
+    path('signup/', views.RegisterUser.as_view(), name = 'signup')
 ]
--- pub_search/main/views.py
+++ pub_search/main/views.py
@@ -1,8 +1,21 @@
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
 
 
 def home(request):
     return render(request, 'main/home.html')
+
+
+
+
+class RegisterUser(CreateView):
+    form_class = UserCreationForm
+    template_name = 'registration/signup.html'
+    success_url = reverse_lazy('login')
 
 
 
